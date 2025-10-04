d20ebe10b7b8c817b282c9b22a78f6dc48ca83bc
bardala <islambardala@gmail.com>
2025-10-04 18:29:29 +0300

    add solve Palindrome problem

--- ProblemSolving/Javascript/codeforces/Palindrome/cf.ts
+++ ProblemSolving/Javascript/codeforces/Palindrome/cf.ts
@@ -1,1 +1,17 @@
 // https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/I
+
+function checkIsPalindrome(str) {
+  let left = 0,
+    right = str.length - 1;
+
+  while (left < right) {
+    if (str[left] !== str[right]) {
+      console.log('NO');
+      return;
+    }
+    left++, right--;
+  }
+
+  console.log('YES');
+  return true;
+}
